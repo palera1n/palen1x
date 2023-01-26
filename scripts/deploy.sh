ada157f944f93582f212c1c75b61cd1ee9f63dc6
flower <97859147+flowerible@users.noreply.github.com>
2023-01-26 08:11:56 -0800

    fix bootstrap option, also remove from menu

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,41 +1,59 @@
-#!/bin/sh
+#!/cores/binpack/bin/sh
 
-if ! iproxy -v >/dev/null 2>&1; then
-echo 'iproxy not installed'
+if stat /.procursus_strapped >/dev/null 2>&1; then
+echo 'WTF.. (Dont install on root)'
 exit
 fi
 
-echo "Downloading resources..."
-IPROXY=$(iproxy 28605 44 >/dev/null 2>&1 & echo $!)
-curl -sLOOOOO https://apt.procurs.us/bootstraps/1900/bootstrap-ssh-iphoneos-arm64.tar.zst \
-https://raw.githubusercontent.com/elihwyma/Pogo/1724d2864ca55bc598fa96bee62acad875fe5990/Pogo/Required/org.coolstar.sileonightly_2.4_iphoneos-arm64.deb \
-https://cdn.discordapp.com/attachments/1028398976640229380/1058313451082100766/ellekit_rootless_fix.deb \
-https://cdn.discordapp.com/attachments/1028398976640229380/1055565626002911253/pspawn.dylib \
-https://cdn.discordapp.com/attachments/1028398976640229380/1056844445892481074/preferenceloader_2.2.6-1debug_iphoneos-arm64.deb
+if stat /var/jb/.procursus_strapped >/dev/null 2>&1; then
+echo 'Already installed.'
+exit
+fi
 
-unzstd bootstrap-ssh-iphoneos-arm64.tar.zst
+if stat /private/preboot/$(cat /private/preboot/active)/procursus >/dev/null 2>&1; then
+echo 'Already installed?'
+exit
+fi
 
-echo "Copying resources to your device..."
-echo "Default password is: alpine"
-scp -qP28605 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" bootstrap-ssh-iphoneos-arm64.tar \
-org.coolstar.sileonightly_2.4_iphoneos-arm64.deb \
-ellekit_rootless_fix.deb \
-pspawn.dylib \
-preferenceloader_2.2.6-1debug_iphoneos-arm64.deb \
-src/iphoneos-arm64/install.sh \
-root@127.0.0.1:/var/root/
+if stat /var/jb >/dev/null 2>&1; then
+echo 'Already installed?'
+exit
+fi
 
+cd /var/root
+curl -sLOOOOO https://apt.procurs.us/bootstraps/1800/bootstrap-ssh-iphoneos-arm64.tar.zst
+curl -sLOOOOO https://raw.githubusercontent.com/elihwyma/Pogo/1724d2864ca55bc598fa96bee62acad875fe5990/Pogo/Required/org.coolstar.sileonightly_2.4_iphoneos-arm64.deb
+curl -sLOOOOO https://cdn.discordapp.com/attachments/1028398976640229380/1056844445892481074/preferenceloader_2.2.6-1debug_iphoneos-arm64.deb
+curl -sLOOOOO https://github.com/dhinakg/ellekit-builder/releases/download/0.2.16.9b188af/ellekit_0.2.16.9b188af_iphoneos-arm64.deb
+zstd -d bootstrap-ssh-iphoneos-arm64.tar.zst
 
-echo "Bootstrapping your device..."
-ssh -qp28605 -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" root@127.0.0.1 "bash /var/root/install.sh"
+sleep 3
 
-rm -rf bootstrap-ssh-iphoneos-arm64.tar.zst
-rm -rf bootstrap-ssh-iphoneos-arm64.tar
-rm -rf org.coolstar.sileonightly_2.4_iphoneos-arm64.deb
-rm -rf ellekit_rootless_fix.deb
-rm -rf pspawn.dylib
-rm -rf preferenceloader_2.2.6-1debug_iphoneos-arm64.deb
+mount -uw /private/preboot
+mkdir /private/preboot/tempdir
+tar --preserve-permissions -xkf bootstrap-ssh-iphoneos-arm64.tar -C /private/preboot/tempdir
+mv -v /private/preboot/tempdir/var/jb /private/preboot/$(cat /private/preboot/active)/procursus
+sleep 1
+ln -s /private/preboot/$(cat /private/preboot/active)/procursus /var/jb
+rm -rf /private/preboot/tempdir
+source /var/jb/etc/profile
+echo balls
+sleep 1
+/var/jb/prep_bootstrap.sh
+/var/jb/usr/libexec/firmware
 
-kill "$IPROXY"
+echo "Installing Sileo-Nightly and upgrading Procursus packages..."
+dpkg -i org.coolstar.sileonightly_2.4_iphoneos-arm64.deb > /dev/null
+uicache -p /var/jb/Applications/Sileo-Nightly.app
+echo balls2
+sleep 1
+apt-get update -o Acquire::AllowInsecureRepositories=true
+apt-get dist-upgrade -y --allow-downgrades --allow-unauthenticated
 
+rm org.coolstar.sileonightly_2.4_iphoneos-arm64.deb
+rm bootstrap-ssh-iphoneos-arm64.tar
+rm bootstrap-ssh-iphoneos-arm64.tar.zst
 
+echo Rebooting...
+sleep 3
+launchctl reboot userspace
