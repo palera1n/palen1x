0f7f3fa6d49e1b67136664c154bc53481e676900
flower <97859147+flowerible@users.noreply.github.com>
2023-05-29 13:09:27 -0700

    chore: remove build numbers

--- build.sh
+++ build.sh
@@ -178,4 +178,4 @@ find . | cpio -oH newc | xz -C crc32 --x86 -vz9eT$(nproc --all) > ../iso/boot/in
 popd
 
 # ISO creation
-grub-mkrescue -o "palen1x-$VERSION-$ARCH.iso" iso --compress=xz
+grub-mkrescue -o "palen1x-$ARCH.iso" iso --compress=xz
