41afcb588620afd276eaa0898c432f5f0affd2e7
ssalggnikool <sam4r16@gmail.com>
2024-03-01 22:22:12 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -55,19 +55,19 @@ if [ "$1" = "RELEASE" ]; then
     case "$ARCH" in
         'amd64')
             ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86_64/alpine-minirootfs-3.17.3-x86_64.tar.gz'
-            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.8/palera1n-linux-x86_64"
+            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.9/palera1n-linux-x86_64"
             ;;
         'i686')
             ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86/alpine-minirootfs-3.17.3-x86.tar.gz'
-            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.8/palera1n-linux-x86"
+            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.9/palera1n-linux-x86"
             ;;
         'aarch64')
             ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/aarch64/alpine-minirootfs-3.17.3-aarch64.tar.gz'
-            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.8/palera1n-linux-arm64"
+            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.9/palera1n-linux-arm64"
             ;;
         'armv7')
             ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/armv7/alpine-minirootfs-3.17.3-armv7.tar.gz'
-            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.8/palera1n-linux-armel"
+            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.9/palera1n-linux-armel"
             ;;
     esac
     echo "INFO: RELEASE CHOSEN"
