93ffdd52bba92b65bd33c0f3102aa2a0049f66e1
CLARATION <claration@riseup.net>
2026-07-24 18:37:39 -0700

    refactor: cleanup script, make action robust
    refactor: cleanup script, make action robust
    
    fix(ci): make it build please
    
    fix(ci): okay now make it work
    
    fix(build): use bash (sigh)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-Linux distro that lets you install <a href="https://github.com/palera1n/palera1n">palera1n<a/>.
+Linux ISO that lets you install <a href="https://github.com/palera1n/palera1n">palera1n<a/>.
 
 [![GitHub Downloads (all assets, all releases)](https://img.shields.io/github/downloads/palera1n/palen1x/total)](https://github.com/palera1n/palen1x/releases)
 [![GitHub Release](https://img.shields.io/github/v/release/palera1n/palen1x)](https://github.com/palera1n/palen1x/releases)
@@ -16,9 +16,8 @@ git clone https://github.com/palera1n/palen1x.git
 cd palen1x
 sudo ./build.sh
 ```
-> You will be prompt to input a version and specify an architecture.
 
-## Credits
+## Acknowledgements
+
 - Asineth for [checkn1x](https://github.com/asineth0/checkn1x)
 - raspberryenvoie for [odysseyn1x](https://github.com/raspberryenvoie/odysseyn1x)
-- [Everyone else who contributed to palen1x](https://github.com/palera1n/palen1x/graphs/contributors)
--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # palen1x build script
-# Made with <3 https://github.com/palera1n/palen1x
-# Modified from https://github.com/asineth0/checkn1x & https://github.com/raspberryenvoie/odysseyn1x :3
-
+# https://github.com/palera1n/palen1x
+#
+# Modified from https://github.com/asineth0/checkn1x
+# Modified from https://github.com/raspberryenvoie/odysseyn1x
 
 [ "$(id -u)" -ne 0 ] && {
     echo 'Please run as root'
@@ -14,32 +15,16 @@ GREEN="$(tput setaf 2)"
 BLUE="$(tput setaf 6)"
 NORMAL="$(tput sgr0)"
 
-while [ -z "$VERSION" ]; do
-    printf 'Version: '
-    read -r VERSION
-done
-
-until [ "$ARCH" = 'x86_64' ] || [ "$ARCH" = 'x86' ] || [ "$ARCH" = 'aarch64' ] || [ "$ARCH" = 'armv7' ]; do
+until [ "$ARCH" = 'x86_64' ] || [ "$ARCH" = 'x86' ]; do
     echo '1 x86_64'
     echo '2 x86'
-    echo '3 aarch64'
-    echo '4 armv7'
-    printf 'Which architecture? x86_64 (default), x86, or aarch64 or armv7: '
+    printf 'Which architecture? x86_64 (default), x86: '
     read -r input_arch
     [ "$input_arch" = 1 ] && ARCH='x86_64'
     [ "$input_arch" = 2 ] && ARCH='x86'
-    [ "$input_arch" = 3 ] && ARCH='aarch64'
-    [ "$input_arch" = 4 ] && ARCH='armv7'
     [ -z "$input_arch" ] && ARCH='x86_64'
 done
 
-apt-get update
-apt-get install -y --no-install-recommends wget gawk debootstrap mtools xorriso ca-certificates curl libusb-1.0-0-dev gcc make gzip xz-utils unzip libc6-dev
-
-download_version=$(curl -s https://api.github.com/repos/palera1n/palera1n/releases | grep -m 1 -o '"tag_name": "[^"]*' | sed 's/"tag_name": "//')
-
-PALERA1N_PREFIX="https://github.com/palera1n/palera1n/releases/download/$download_version/palera1n-linux-"
-USBMUXD_PREFIX="https://cdn.nickchan.lol/palera1n/artifacts/usbmuxd-static/usbmuxd-linux-"
 PATH_VERSION="v3.20"
 ROOTFS_VERSION="3.20.0"
 ROOTFS_PREFIX="https://dl-cdn.alpinelinux.org/alpine/${PATH_VERSION}/releases"
@@ -47,24 +32,10 @@ ROOTFS_PREFIX="https://dl-cdn.alpinelinux.org/alpine/${PATH_VERSION}/releases"
 case "$ARCH" in
     'x86_64')
         ROOTFS="${ROOTFS_PREFIX}/x86_64/alpine-minirootfs-${ROOTFS_VERSION}-x86_64.tar.gz"
-        PALERA1N="${PALERA1N_PREFIX}x86_64"
-        USBMUXD="${USBMUXD_PREFIX}x86_64"
         ;;
     'x86')
         ROOTFS="${ROOTFS_PREFIX}/x86/alpine-minirootfs-${ROOTFS_VERSION}-x86.tar.gz"
-        PALERA1N="${PALERA1N_PREFIX}x86"
-        USBMUXD="${USBMUXD_PREFIX}x86"
         ;;
-    'aarch64')
-        ROOTFS="${ROOTFS_PREFIX}/aarch64/alpine-minirootfs-${ROOTFS_VERSION}-aarch64.tar.gz"
-        PALERA1N="${PALERA1N_PREFIX}arm64"
-        USBMUXD="${USBMUXD_PREFIX}arm64"
-        ;;
-    'armv7')
-        ROOTFS="${ROOTFS_PREFIX}/armv7/alpine-minirootfs-${ROOTFS_VERSION}-armv7.tar.gz"
-        PALERA1N="${PALERA1N_PREFIX}armel"
-        USBMUXD="${USBMUXD_PREFIX}armel"
-    ;;
 esac
 
 echo $PALERA1N
@@ -91,11 +62,13 @@ http://dl-cdn.alpinelinux.org/alpine/edge/testing
 
 sleep 2
 
+ROOTFS_PATH="/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin"
+
 #
-cat << ! | chroot rootfs /usr/bin/env PATH=/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin /bin/sh
+cat << ! | chroot rootfs /usr/bin/env PATH=$ROOTFS_PATH /bin/sh
 apk update
 apk upgrade
-apk add bash alpine-base ncurses udev openssh-client sshpass newt
+apk add bash alpine-base ncurses udev openssh-client sshpass usbmuxd mandoc man-pages
 apk add --no-scripts linux-lts linux-firmware-none
 rc-update add bootmisc
 rc-update add hwdrivers
@@ -113,7 +86,7 @@ kernel/drivers/hid/hid-cherry.ko
 kernel/drivers/hid/hid-apple.ko
 kernel/net/ipv4
 !
-chroot rootfs /usr/bin/env PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin \
+chroot rootfs /usr/bin/env PATH=$ROOTFS_PATH \
 	/sbin/mkinitfs -F "palen1x" -k -t /tmp -q $(ls rootfs/lib/modules)
 rm -rf rootfs/lib/modules
 mv -v rootfs/tmp/lib/modules rootfs/lib
@@ -125,32 +98,34 @@ depmod -b rootfs $(ls rootfs/lib/modules)
 
 # create config shit
 echo 'palen1x' > rootfs/etc/hostname
-echo "PATH=$PATH:$HOME/.local/bin" > rootfs/root/.bashrc # d
-echo "export PALEN1X_VERSION='$VERSION'" > rootfs/root/.bashrc
-echo '/usr/bin/palen1x_menu' >> rootfs/root/.bashrc
-echo "Rootless" > rootfs/usr/bin/.jbtype
-echo "-l" > rootfs/usr/bin/.args
+echo "PATH=$ROOTFS_PATH" > rootfs/root/.bashrc
+echo 'welcome' >> rootfs/root/.bashrc
 
 # Unmount fs
 umount -v rootfs/{dev,sys,proc}
 
-#
-curl -Lo rootfs/usr/bin/palera1n "$PALERA1N"
-chmod +x rootfs/usr/bin/palera1n
+# install inetcat
+cp ../resources/inetcat rootfs/bin/inetcat
+cp ../resources/inetcat-LICENSE rootfs/usr/share/licenses/
 
-curl -Lo rootfs/usr/sbin/usbmuxd "$USBMUXD"
-chmod +x rootfs/usr/sbin/usbmuxd
+# install palera1n
+if [ -f ../resources/palera1n.tar.gz ]; then
+    tar -xzf ../resources/palera1n.tar.gz -C rootfs/usr/
+elif [ -f ../resources/palera1n ]; then
+    cp ../resources/palera1n rootfs/bin/palera1n
+else
+    echo "Error: Neither ./resources/palera1n.tar.gz nor ./resources/palera1n exists." >&2
+    exit 1
+fi
 
-cp -av ../inittab rootfs/etc
-cp -v ../scripts/* rootfs/usr/bin
-chmod -v 755 rootfs/usr/local/bin/*
+cp -av ../scripts/inittab rootfs/etc
+cp -v ../scripts/* rootfs/bin
 ln -sv sbin/init rootfs/init
 ln -sv ../../etc/terminfo rootfs/usr/share/terminfo # fix ncurses
 
 cp -av rootfs/boot/vmlinuz-lts iso/boot/vmlinuz
 cat << ! > iso/boot/grub/grub.cfg
 insmod all_video
-echo 'palen1x $VERSION'
 linux /boot/vmlinuz  quiet loglevel=3
 initrd /boot/initramfs.xz
 boot
@@ -161,5 +136,12 @@ rm -rf tmp/* boot/* var/cache/* etc/resolv.conf
 find . | cpio -oH newc | xz -C crc32 --x86 -vz9eT$(nproc --all) > ../iso/boot/initramfs.xz
 popd
 
-# ISO creation
-grub-mkrescue -o "palen1x-$ARCH-$VERSION.iso" iso --compress=xz
+# naming
+OUTPUT="palen1x-$ARCH"
+
+if [ -n "$VERSION" ]; then
+  OUTPUT="palen1x-$VERSION-$ARCH"
+fi
+
+# create iso
+grub-mkrescue -o "$OUTPUT.iso" iso --compress=xz
